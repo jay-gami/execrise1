21ae10974cb4cb40d4a9e8d696e43a7007c94efb
jay-gami <jay.gami@softices.com>
2023-02-13 12:11:49 +0530

    new

--- file.rb
+++ file.rb
@@ -1,8 +1,10 @@
+#Write a program to get a list of all file/directory names from the given.	
+
 class Directory
     def file
-a= Dir::entries( "/home/scpl/Desktop/exercise")
-puts a
-end
+        a= Dir::entries( "/home/scpl/Desktop/exercise")
+        puts a
+    end
 end
 
 obj=Directory.new()
--- operator.rb
+++ operator.rb
@@ -1,24 +1,26 @@
-#write a program to allow user to input two integer value and then program prints the results of
-# adding,subtracting,multiplying and dividing among the two values
+=begin
+write a program to allow user to input two integer value and then program prints the results of
+adding,subtracting,multiplying and dividing among the two values
+=end
 
 class Arithmetic
-def operator
-    puts "Enter first value"
-    a=gets.chomp.to_i
-    puts "Enter second value"
-    b=gets.chomp.to_i
+    def operator
+        puts "Enter first value"
+        a=gets.chomp.to_i
+        puts "Enter second value"
+        b=gets.chomp.to_i
 
 
-    oper_add=a+b
-    oper_sub=a-b
-    oper_mul=a*b
-    oper_div=a/b
+        oper_add=a+b
+        oper_sub=a-b
+        oper_mul=a*b
+        oper_div=a/b
 
-    puts "addition of two number is : #{oper_add}"
-    puts "subtraction of two number is : #{oper_sub}"
-    puts "multiplication of two number is : #{oper_mul}"
-    puts "division of two number is : #{oper_div}"
-end
+        puts "addition of two number is : #{oper_add}"
+        puts "subtraction of two number is : #{oper_sub}"
+        puts "multiplication of two number is : #{oper_mul}"
+        puts "division of two number is : #{oper_div}"
+    end
 end
 
 obj=Arithmetic.new()
