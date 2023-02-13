5da9afc8d69334576d5176f91f25f194de9e13d0
jay-gami <jay.gami@softices.com>
2023-02-13 14:04:48 +0530

    new

--- divisible.rb
+++ divisible.rb
@@ -3,27 +3,17 @@ class Divisible
         n1,n2,n3=gets.chomp.split(' ').map(&:to_i)
         range=(n1..n2)
         count=0
-        puts "Numbers:"
-        (range).each do |i|
+        print "Numbers: "
+        (range).each do |i| 
             if i%n3==0
-                puts  "#{i}"
-                count =count+1
-            end
+                print  "#{i} "
+             count =count+1
+            end         
         end
+    
+        puts "& Count: #{count}"
 
-        puts "Counts: #{count}"   
 
-=begin
-        for i in range do
-            if i%n3==0
-                puts "#{i}"
-                count = count+1
-            end
-        end
-
-        puts "#{count}"
-
-=end
 
     end
 end
