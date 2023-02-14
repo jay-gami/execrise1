e0f1d58181b4542b3be94ca7ea6cc5935702d35e
jay-gami <jay.gami@softices.com>
2023-02-14 17:22:48 +0530

    new

--- website_image.rb
+++ website_image.rb
@@ -1,29 +1,27 @@
 class Website
     def initialize
         @x=gets.chomp.to_i
-        @y=gets.chomp.to_i
-       
-       
+        @y=gets.chomp.to_i 
     end
+
     def process
-# @l=@h*@w
-@i=0
-while @i<@y do
-    @h,@w=gets.chomp.split(' ').map(&:to_i)
- @i +=1
-end
+     @i=0   
+     while @i<@y do
+        @h,@w=gets.chomp.split(' ').map(&:to_i)
+        @i +=1
+     end
 
 
-    if(@x<@h)
+     if(@x<@h)
         puts "not valid"
-    elsif(@w==@h)
+     elsif(@w==@h)
         puts "perfect"
-    else(@w!=@h)
+     else(@w!=@h)
         puts "crop and accept"
-    end 
+     end 
     
 
-end
+    end
 
 
 
