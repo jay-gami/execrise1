8081b52f260a441d6ed27e2b100a239a4114b8cf
jay-gami <jay.gami@softices.com>
2023-02-15 18:29:55 +0530

    new

--- website_image.rb
+++ website_image.rb
@@ -1,36 +1,46 @@
 class Website
-    def initialize
-      @l=gets.chomp.to_i
-      @y=gets.chomp.to_i 
-    end
-
-    def process
-      @arr=[]
-
-     @i=0   
-      while (@i<@y) do
-
-        @w,@h=gets.chomp.split(' ').map(&:to_i)
-        @i =@i+1  
-        @arr.push(@w,@h)
-      @j=0
-      while (@j<@y) do
-
-
-         if(@w<@l || @h<@l)
-            if
-            puts "not valid"
-            
-            end
-         elsif(@w==@h)
-         puts "Accepted"
-         
-         else(@w!=@h)
-         puts "Crop and Accept"
-        
-         end
+   def initialize
+     @l=gets.chomp.to_i
+     @y=gets.chomp.to_i 
    end
+
+   def process
+     @arr=[]
+     @hash=[]
+    @i=0   
+     while (@i<@y) do
+
+       @w,@h=gets.chomp.split(' ').map(&:to_i)
+       @i =@i+1  
+       @arr.push(@w,@h)
+   
+        if(@w<@l || @h<@l)
+         @hash.push("Invalid")
+        next
+        elsif(@w==@h)
+         @hash.push("perfect")
+      next
+        else (@w!=@h)
+         @hash.push("Crop and Accept")
+      end
+   end
+
+# puts @arr
+puts "#{@hash}" 
+# puts "#{@arr}"
+@h=Hash[*@hash, @i+0]
+# puts "#{@h.fetch(@i)}"
+# @q=Hash[keys.zip[@i]]
+puts "#{@h}"
+
+
+# @m=@z.values
+# puts @m
+
 end
+
+   
+   
 
 
 
