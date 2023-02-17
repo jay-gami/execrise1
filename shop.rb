6ca01572bb6547b5b5a9fdb6297835ca567eb9f5
jay-gami <jay.gami@softices.com>
2023-02-17 18:00:39 +0530

    new like only loop and date remaining

--- shop.rb
+++ shop.rb
@@ -1,64 +1,120 @@
+require 'date'
+$total_order=0
+$total=0
+$maximum=0
+$minimum=0
+$avg=0
+$arr1=[]
+$arr2=[]
+
+
 class Shop
     def initialize
         puts "==============="
         puts "Welcome to shop"
         puts "==============="
-        @time1=Time.now
+        @time1=Time.new
         puts @time1.strftime("%Y-%m-%d %H:%M:%S")
         puts "---------------"
-
-        puts "Total Order #:#{@order}"
       
-        puts "Total Amount :#{@amt}"
-       
-        puts "Minimum Order:#{@minimum}"
-        puts "Maximum Order:#{@maximum}"
-        puts "Average Order:#{@avg}"
+        puts "Total Order #:#{$total_order}"
+        puts "Total Amount :#{$arr1.sum}"
+        puts "Minimum Order:#{$minimum}"
+        puts "Maximum Order:#{$maximum}"
+        puts "Average Order:#{$avg}"
         puts "==============="
-
-
-
-    puts "1 Create New Order  "
-    puts "2 Print Day Details "
-    puts "3 Print Month Details "
-    puts "How may I help you?(1,2,3 or quit)"
-    
-    
-  
-    
-    end
-
-    def process
-   @a=0
-   @q=0
-   while(@a<4)do
-
-    @a=@a+1
-    @a=gets.chomp.to_i
-
-    if(@a==1)
-
-        puts "create new order"
         
+         
+        puts "1 Create New Order  "
+        puts "2 Print Day Details "
+        puts "3 Print Month Details "
+        puts "How may I help you?(1,2,3 or quit)"
+    end
+    def process   
+          
+        loop  do 
+        @a=gets.chomp.to_i
+   
 
-    elsif(@a==2)
+    if(@a==1)  
+        $total_order=$total_order+1
+        puts "create new order"
+       
+        $amt=gets.chomp.to_i
+
+         if ($amt==0)
+          $total_order=$total_order-1
+          puts "1 Create New Order  "
+          puts "2 Print Day Details "
+          puts "3 Print Month Details "
+          puts "How may I help you?(1,2,3 or quit)"
+          next
+         
+        
+         end
+        $arr1.push($amt)
+        $total= $arr1.sum
+        $maximum=$arr1.max
+        $minimum=$arr1.min
+        $avg=$arr1.sum/$arr1.size
+
+
+
+        puts "total:#{$total}"
+        puts "max:#{$maximum}"
+        puts "min:#{$minimum}"
+        puts "total Order:#{$total_order}"
+        puts "avg:#{$avg}"  
+          
+        if ($total_order==$total_order)
+          puts "1 Create New Order  "
+          puts "2 Print Day Details "
+          puts "3 Print Month Details "
+          puts "How may I help you?(1,2,3 or quit)"
+        end
+        elsif(@a==2)
+        $total_order=$total_order+1
 
         puts "Print Day Details"
+        @day,@month,@year=gets.chomp.split('-').map(&:to_i)
+
+        $arr2.push([@year,@month,@day])
+
+        # puts $arr2[0]
+        # $arr2.strptime("%B %Y")
+
+        puts "#{$arr2}"
+        for j in $arr2
+          puts j[0]
+        end
+        if ($total_order==$total_order)
+          puts "1 Create New Order  "
+          puts "2 Print Day Details "
+          puts "3 Print Month Details "
+          puts "How may I help you?(1,2,3 or quit)"
+         end
+
 
     elsif(@a==3)
+    puts "Print Month details"  
+    @month,@year=gets.chomp.split('-').map(&:to_i)
 
-        puts "Print Month details"
-        3
-    else(@a==@q)
-
+    @s=Date.new(@year,@month) 
+    puts @s
+    if ($total_order==$total_order)
+      puts "1 Create New Order  "
+      puts "2 Print Day Details "
+      puts "3 Print Month Details "
+      puts "How may I help you?(1,2,3 or quit)"
+     end
+    
+    else(@a=='q' || @a=='quit')
+ 
         puts "exit"
-    break  
-    end
-       
+        break
         end
-    
-    
 
+    end
     end
 end
 
--- website_image.rb
+++ website_image.rb
@@ -1,49 +1,31 @@
 class Website
-   def initialize
-     @l=gets.chomp.to_i
-     @y=gets.chomp.to_i 
-   end
-
-   def process
-     @arr=[]
-     @hash=[]
-    @i=0   
-     while (@i<@y) do
-
-       @w,@h=gets.chomp.split(' ').map(&:to_i)
-       @i =@i+1  
-       @arr.push(@w,@h)
-   
-        if(@w<@l || @h<@l)
-         @hash.push("Invalid")
+  def initialize
+  @l=gets.chomp.to_i
+  @y=gets.chomp.to_i
+  end
+  
+  def process
+    @arr1=[]
+    @i=0
+    while (@i<@y) do
+      @w,@h=gets.chomp.split(' ').map(&:to_i)
+      if(@w<@l || @h<@l)
+         @i = @i+1
+         @arr1.push(Hash[@i,"Invalid"])
         next
         elsif(@w==@h)
-         @hash.push("perfect")
-      next
+        @i = @i + 1
+        @arr1.push(Hash[@i,"perfect"])
+        next
         else (@w!=@h)
-         @hash.push("Crop and Accept")
-      end
-   end
-
-# puts @arr
-puts "#{@hash}" 
-# puts "#{@arr}"
-@h=Hash[*@hash, @i+0]
-# puts "#{@h.fetch(@i)}"
-# @q=Hash[keys.zip[@i]]
-puts "#{@h}"
-
-
-# @m=@z.values
-# puts @m
-
-end
-
-   
-   
-
-
-
-end
-obj=Website.new()
-obj.process
+        @i = @i + 1
+        @arr1.push(Hash[@i,"Crop and Accept"])
+      end  
+    end
+    for j in @arr1
+      puts j.values
+    end
+  end
+  end
+  obj=Website.new()
+  obj.process
